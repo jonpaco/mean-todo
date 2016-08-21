8e5f3fff98418e11e577b57d892f75ea33e062a3
Jonathan Cardinale <jonathan.cardinale87@gmail.com>
2016-08-21 12:53:43 -0400

    Added some front end edit capabilities.

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,5 +1,6 @@
 angular.module('meanTodoApp')
 .controller('MainCtrl', function ($scope, TodoService) {
+    // Runs everytime the main controller gets called.
     TodoService.getTodos(function(response){
         var todos = response.data.todos;
         $scope.todos = todos;
